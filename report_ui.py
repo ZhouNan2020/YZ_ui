8a54b00249ecb6f6f87795e0d487d68cf2a6f659
Zhou <zhounan@1utar.my>
2023-05-11 10:17:44 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -2,6 +2,16 @@
 import streamlit as st
 
 
-# 添加html界面标题为“优卓医药科技”
-st.markdown("<h1 style='text-align: center; color: red;'>优卓医药科技</h1>", unsafe_allow_html=True)
+# 定义一个包含了@cache的testforcache类, 在这个类中的第二个方法使用st的数字属于框获取数字并相加，测试cache的效果
+class testforcache:
+    def __init__(self):
+        pass
+
+    @st.cache
+    def test1(self):
+        return 1
+
+    def test2(self):
+        a = st.number_input('input a number')
+        return a + self.test1()
 
