9c6981a78a3f2522e0aab3c9ee24f5952d1378c3
Zhou <zhounan@1utar.my>
2023-05-11 10:12:16 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -1,6 +1,6 @@
 # import streamlit和其它的处理word的库
 import streamlit as st
-import docx
+
 
 # 添加html界面标题为“优卓医药科技”
 st.markdown("<h1 style='text-align: center; color: red;'>优卓医药科技</h1>", unsafe_allow_html=True)
