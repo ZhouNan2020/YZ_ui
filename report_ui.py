d8c467412755a3b0b5a7e44a40bdbcf27231653e
Zhou <zhounan@1utar.my>
2023-05-11 11:09:34 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -10,29 +10,33 @@ import streamlit as st
 
 st.set_page_config(page_title="优卓医药科技", page_icon="🧊", layout="wide")
 
-# 定义一个class，在侧栏用于上传excel数据文件，并且要展示这个excel的文件名和每一个sheet的名称，使sheet名称可被选中。注意使用@st.cache
-# 用于上传excel文件的类
-class UploadFile:
+# 定义一个class，在st.sidebar中中用于上传excel，在上传之后，在sidebar中显示上传的文件名，并且显示excel中的sheet名
+class FileUploader:
     def __init__(self):
         self.file = None
         self.sheet = None
-        self.sheet_name = None
+        self.sheet_names = None
 
     def upload(self):
-        self.file = st.file_uploader("上传文件", type=["xlsx", "xls"])
-        if self.file is not None:
-            self.sheet_name = [i for i in self.file.sheet_names]
-            self.sheet = st.selectbox("选择工作表", self.sheet_name)
-            return self.file, self.sheet
+        self.file = st.sidebar.file_uploader(
+            label="上传excel文件",
+            type=["xlsx", "xls"],
+            accept_multiple_files=False,
+            key="file_uploader",
+        )
+        if self.file:
+            self.sheet_names = self.get_sheet_names()
+            self.sheet = st.sidebar.selectbox(
+                label="选择sheet", options=self.sheet_names, key="sheet"
+            )
+        return self.file, self.sheet
 
-    def get_file(self):
-        return self.file
+    def get_sheet_names(self):
+        import pandas as pd
 
-    def get_sheet(self):
-        return self.sheet
+        df = pd.ExcelFile(self.file)
+        return df.sheet_names
 
-    def get_sheet_name(self):
-        return self.sheet_name
-
-# 实例化
-upload_file = UploadFile()
+# 实例化并调用
+file_uploader = FileUploader()
+file, sheet = file_uploader.upload()
