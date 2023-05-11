4e01b1b46904993e2f5dee8e954b7154482b24a7
Zhou <zhounan@1utar.my>
2023-05-11 10:39:44 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -5,8 +5,7 @@ import streamlit as st
 # 在整个脚本中，能够使用@cache缓存的函数一定要用@st.cache
 # 用于缓存函数的返回值，避免st频繁刷新
 # 项目标题“优卓医药科技”
-@st.cache(suppress_st_warning=True)
-def title():
-    st.title("优卓医药科技")
-# 调用标题
-title()
+
+st.title("优卓医药科技")
+
+
