93fe32fe42e7fbc0f7ee4a6c0d6a6e7406feed17
Zhou <zhounan@1utar.my>
2023-05-11 10:21:50 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -2,18 +2,18 @@
 import streamlit as st
 
 
-# 定义一个包含了@cache的testforcache类, 在这个类中的第二个方法使用st的数字属于框获取数字并相加，测试cache的效果
+# 定义一个包含了@cache的testforcache类, 在这个类中的第二个方法使用st的数字输入框获取2个数字并与init的数字相加，测试cache的效果
 class testforcache:
     def __init__(self):
-        pass
+        self.a = 1
+        self.b = 2
 
     @st.cache
-    def test1(self):
-        return 1
-
     def test2(self):
-        a = st.number_input('input a number')
-        return a + self.test1()
+        self.a = st.number_input('a', value=self.a)
+        self.b = st.number_input('b', value=self.b)
+        return self.a + self.b
+    
 
 # 实例化testforcache类
 test = testforcache()
