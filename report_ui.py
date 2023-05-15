fe4b93d3b79f69b82a9a0c9afc45cf86d576d129
Zhou <zhounan@1utar.my>
2023-05-15 11:39:34 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -92,9 +92,10 @@ class DescriptiveStatistics(DataPrepare):
 
     def descriptive_statistics(self):
         # 调用父类的descriptive_select_columns方法
-        selected_data, selected_columns = self.descriptive_select_columns()
+        selected_data, selected_columns = super().descriptive_select_columns()
         # 给一个button，用于触发描述性统计的计算
         if st.button("开始分析"):
+            
             # 描述性统计的结果赋值给descriptive_statistics_result
             descriptive_statistics_result = selected_data.describe()
             # st.dataframe显示描述性统计的结果
