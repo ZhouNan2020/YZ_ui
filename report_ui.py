de4a8976987657f633e82b0d745d7d80e28df933
ZhouNan <zhounan@1utar.my>
2023-06-28 14:31:14 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -1109,12 +1109,12 @@ class MyApp:
             wbc_dict = {k: v[['白细胞']] for k, v in blood_dict.items()}
             # 将wbc_dict中的df合并为一个df，并且按照顺序在合并后的列名前加上“访视[i]”，名为wbc_df
             wbc_df = pd.concat(wbc_dict.values(), axis=1)
-            wbc_df.columns = [f'访视{i+1}_{col}' for i, col in enumerate(wbc_df.columns)]
+            wbc_df.columns = [f'访视{i+1}' for i, col in enumerate(wbc_df.columns)]
             # 使用箱型图表示wbc_df中每一列数据，将所有箱放入同一个坐标轴，x轴为wbc_df的列名，y轴为数值，最后使用st.pyplot展示该图
             fig, ax = plt.subplots(figsize=(10, 6))
             sns.boxplot(data=wbc_df, ax=ax)
             ax.set_xticklabels(wbc_df.columns, rotation=45, ha='right',fontproperties=font)
-            ax.set_title('白细胞箱型图',fontproperties=font)
+            ax.set_title('访视期间白细胞变化情况',fontproperties=font)
             # 使中文正常显示
             
             
