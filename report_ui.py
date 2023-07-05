41f6b5110d44dcc83e045290997b59c19158478e
ZhouNan <zhounan@1utar.my>
2023-07-05 15:39:06 +0800

    Update report_ui.py

--- report_ui.py
+++ report_ui.py
@@ -1587,7 +1587,7 @@ class MyApp:
                         st.write(mediation_model.summary())
                         # 输出中介效应和总效应，使用st.dataframe
                         
-                        result_df = pd.DataFrame({'中介效应': [indirect_effect], '总效应': [total_effect]})
+                        
                         for col in med_independent:
                             result_df[f'{col}系数'] = mediation_model.params[col]
                         result_df['中介变量系数'] = mediation_model.params['PreM']
