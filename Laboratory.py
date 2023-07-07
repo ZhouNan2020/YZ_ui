1e3eea2f32fc75342a0915cb4fb80be7a3939579
ZhouNan <zhounan@1utar.my>
2023-07-07 18:02:26 +0800

    Update Laboratory.py

--- Laboratory.py
+++ Laboratory.py
@@ -67,6 +67,11 @@ if file is not None:
         # 提取所有包含”#血常规“但是不包括”#血常规其他异常结果“的key
         if '#血常规' in sheet and '#血常规其他异常结果' not in sheet:
             tab16_dict_blood[sheet] = tab16_dict[sheet]
+    # 遍历tab16_dict_blood中的每一个df，将每个df中的“+”，“UK","uk"，“uK","Uk","-"替换为np.nan
+    for sheet in tab16_dict_blood.keys():
+        tab16_dict_blood[sheet].replace(['\+', 'UK', 'uk', 'uK', 'Uk', '\-'], np.nan, inplace=True, regex=True)
+
+
     # 根据”label“列的值不同进行分组计算tab16_dict_blood中每一个df中”白细胞计数“列的非空值计数、空值计数、平均值、标准差，中位数，Q1，Q3，最小值，最大值，形成一个新的df，存入新的dict中
     # 创建一个新的字典来存储结果
     result_dict = {}
@@ -231,14 +236,15 @@ if file is not None:
 
 
     
-        # 从tab16_dict中提取出所有key名包含”#血常规“的df，形成一个新的dict
+    # 从tab16_dict中提取出所有key名包含”#血常规“的df，形成一个新的dict
     tab16_dict_bloche = {}
     for sheet in tab16_dict.keys():
         # 提取所有包含”#血常规“但是不包括”#血常规其他异常结果“的key
         if '#血生化' in sheet and '#血生化其他异常结果' not in sheet:
             tab16_dict_bloche[sheet] = tab16_dict[sheet]
-
-
+    # 遍历tab16_dict_bloche中的每一个df，将每个df中的“+”，“UK","uk"，“uK","Uk","-"替换为np.nan
+    for sheet in tab16_dict_bloche.keys():
+        tab16_dict_bloche[sheet].replace(['\+', 'UK', 'uk', 'uK', 'Uk', '\-'], np.nan, inplace=True, regex=True)
     # 根据”label“列的值不同进行分组计算tab16_dict_bloche中每一个df中”谷丙转氨酶(ALT)“列的非空值计数、空值计数、平均值、标准差，中位数，Q1，Q3，最小值，最大值，形成一个新的df，存入新的dict中
     # 创建一个新的字典来存储结果
     result_dict_5 = {}
@@ -364,6 +370,80 @@ if file is not None:
     # 遍历tab16_dict_bloche中的每一个df
     for sheet in tab16_dict_bloche.keys():
         # 获取当前df
+        df = tab16_dict_bloche[sheet]
+        # 根据"label"列的值进行分组
+        grouped = df.groupby('label')
+        # 计算每个组中"超敏C反应蛋白（hs-CRP）"列的非空值计数、空值计数、平均值、标准差，中位数，Q1，Q3，最小值，最大值
+        result = grouped['超敏C反应蛋白（hs-CRP）'].agg(['count', 'mean', 'std', 'median', lambda x: x.quantile(0.25), lambda x: x.quantile(0.75), 'min', 'max'])
+        # 将空值计数添加到结果中
+        result['null_count'] = grouped['超敏C反应蛋白（hs-CRP）'].apply(lambda x: x.isnull().sum())
+        # 更改result的列名为统计值对应的名字
+        result.columns = ['非空值计数', '平均值', '标准差', '中位数', 'Q1', 'Q3', '最小值', '最大值', '空值计数']
+        # 把空值计数放到第二列
+        result = result[['非空值计数', '空值计数', '平均值', '标准差', '中位数', 'Q1', 'Q3', '最小值', '最大值']]
+        # 创建一个新的df来存储检验结果
+        test_result = pd.DataFrame(columns=['检验的变量', '检验方法', '统计量', 'p值'])
+        # 使用卡方检验比较两组非空值计数和空值计数的差异
+        try:
+            chi2, p_chi2 = stats.chi2_contingency(result[['非空值计数', '空值计数']].values)[:2]
+            test_result = pd.concat([test_result, pd.DataFrame({'检验的变量': ['非空值计数和空值计数'], '检验方法': ['卡方检验'], '统计量': [chi2], 'p值': [p_chi2]})], ignore_index=True)
+        except ValueError:
+            pass
+        # 使用t检验对比原始df中两组数值的差异
+        try:
+            t, p_t = stats.ttest_ind(df[df['label'] == '试验组']['超敏C反应蛋白（hs-CRP）'].dropna(), df[df['label'] == '对照组']['超敏C反应蛋白（hs-CRP）'].dropna())
+            test_result = pd.concat([test_result, pd.DataFrame({'检验的变量': ['超敏C反应蛋白（hs-CRP）'], '检验方法': ['t检验'], '统计量': [t], 'p值': [p_t]})], ignore_index=True)
+        except ValueError:
+            pass
+        # 将检验结果存入新的dict中
+        result_dict_8[sheet + '_test'] = test_result
+        # 将结果存入新的dict中
+        result_dict_8[sheet] = result
+        st.write(sheet)
+        st.write(result_dict_8[sheet])
+        st.write(result_dict_8[sheet + '_test'])
+
+
+    # 根据”label“列的值不同进行分组计算tab16_dict_bloche中每一个df中”肌酐（Crea）“列的非空值计数、空值计数、平均值、标准差，中位数，Q1，Q3，最小值，最大值，形成一个新的df，存入新的dict中
+    # 创建一个新的字典来存储结果
+    result_dict_9 = {}
+    # 遍历tab16_dict_bloche中的每一个df
+    for sheet in tab16_dict_bloche.keys():
+        # 获取当前df
+        df = tab16_dict_bloche[sheet]
+        # 根据"label"列的值进行分组
+        grouped = df.groupby('label')
+        # 计算每个组中"肌酐（Crea）"列的非空值计数、空值计数、平均值、标准差，中位数，Q1，Q3，最小值，最大值
+        result = grouped['肌酐（Crea）'].agg(['count', 'mean', 'std', 'median', lambda x: x.quantile(0.25), lambda x: x.quantile(0.75), 'min', 'max'])
+        # 将空值计数添加到结果中
+        result['null_count'] = grouped['肌酐（Crea）'].apply(lambda x: x.isnull().sum())
+        # 更改result的列名为统计值对应的名字
+        result.columns = ['非空值计数', '平均值', '标准差', '中位数', 'Q1', 'Q3', '最小值', '最大值', '空值计数']
+        # 把空值计数放到第二列
+        result = result[['非空值计数', '空值计数', '平均值', '标准差', '中位数', 'Q1', 'Q3', '最小值', '最大值']]
+        # 创建一个新的df来存储检验结果
+        test_result = pd.DataFrame(columns=['检验的变量', '检验方法', '统计量', 'p值'])
+        # 使用卡方检验比较两组非空值计数和空值计数的差异
+        try:
+            chi2, p_chi2 = stats.chi2_contingency(result[['非空值计数', '空值计数']].values)[:2]
+            test_result = pd.concat([test_result, pd.DataFrame({'检验的变量': ['非空值计数和空值计数'], '检验方法': ['卡方检验'], '统计量': [chi2], 'p值': [p_chi2]})], ignore_index=True)
+        except ValueError:
+            pass
+        # 使用t检验对比原始df中两组数值的差异
+        try:
+            t, p_t = stats.ttest_ind(df[df['label'] == '试验组']['肌酐（Crea）'].dropna(), df[df['label'] == '对照组']['肌酐（Crea）'].dropna())
+            test_result = pd.concat([test_result, pd.DataFrame({'检验的变量': ['肌酐（Crea）'], '检验方法': ['t检验'], '统计量': [t], 'p值': [p_t]})], ignore_index=True)
+        except ValueError:
+            pass
+        # 将检验结果存入新的dict中
+        result_dict_9[sheet + '_test'] = test_result
+        # 将结果存入新的dict中
+        result_dict_9[sheet] = result
+        st.write(sheet)
+        st.write(result_dict_9[sheet])
+        st.write(result_dict_9[sheet + '_test'])
+
+    # 
 
 
 
