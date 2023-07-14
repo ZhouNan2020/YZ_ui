454fffef2c16ea6d4cf565876481c7629da523e5
ZhouNan <zhounan@1utar.my>
2023-07-14 14:40:23 +0800

    Update codestra.py

--- codestra.py
+++ codestra.py
@@ -321,18 +321,7 @@ if file is not None:
     
 
     
-    with pd.ExcelWriter('病程数据集.xlsx') as writer:  
-            new_df_4.to_excel(writer, sheet_name='咽喉炎')
-            new_df_5.to_excel(writer, sheet_name='咽炎')
-            new_df_6.to_excel(writer, sheet_name='扁桃体炎')
-    st.download_button(
-        label="Download data as Excel",
-        data=open('病程数据集.xlsx', 'rb').read(),
-        file_name="病程数据集.xlsx",
-        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        )
-
-
+   
    
 
 
